c444301558d34b592cb5a092ddd6d4ca3874bb22
Lautaro Garavano <lgaravano28@gmail.com>
2021-05-02 19:39:47 -0300

    ehh creo que termine todo xd

--- tp1.hs
+++ tp1.hs
@@ -1,5 +1,5 @@
-import Data.List
-import Data.Maybe
+import Data.List ( maximumBy, minimumBy, findIndex, sortBy )
+import Data.Maybe ( fromMaybe )
 
 -- Ejercicio 1
 
@@ -12,8 +12,8 @@ class Punto p where
     dist :: p -> p -> Double
     dist p1 p2 = sum [(coord i p2 - coord i p1)^ 2 | i <- [0..dimension p1 - 1]]
 
-newtype Punto2d = P2d (Double, Double) deriving Show
-newtype Punto3d = P3d (Double, Double, Double) deriving Show
+newtype Punto2d = P2d (Double, Double) deriving (Eq, Show)
+newtype Punto3d = P3d (Double, Double, Double) deriving (Eq, Show)
 
 instance Punto Punto2d where
     dimension _ = 2
@@ -26,10 +26,13 @@ instance Punto Punto3d where
     coord 1 (P3d (_, y, _)) = y
     coord 2 (P3d (_, _, z)) = z
 
--- EJERCICIO 2
+-- Ejercicio 2
+
+comparar :: Punto p => Int -> p -> p -> Ordering
+comparar eje a b = compare (coord eje a) (coord eje b)
 
 ordenar :: Punto p => Int -> [p] -> [p]
-ordenar eje = sortBy (\a b -> compare (coord eje a) (coord eje b))
+ordenar eje = sortBy (comparar eje)
 
 mayorMediana :: Punto p => [p] -> Int -> Int
 mayorMediana lista eje = fromMaybe 0 (findIndex (\p -> coord eje p > mediana) lista) - 1
@@ -52,5 +55,67 @@ fromList = generarArbol  0
 -- Node (Node Empty (P2d (1.0,2.0)) (Node Empty (P2d (1.0,3.0)) Empty 0) 1) (P2d (1.0,4.0)) (Node Empty (P2d (2.0,3.0)) (Node Empty (P2d (3.0,3.0)) Empty 0) 1) 0
 -- Node (Node (Node Empty (P2d (1.0,2.0)) Empty 1) (P2d (1.0,3.0)) (Node Empty (P2d (2.0,3.0)) Empty 1) 0) (P2d (3.0,3.0)) (Node Empty (P2d (1.0,4.0)) Empty 0) 1
 
+-- Ejercicio 3
+
 insertar :: Punto p => p -> NdTree p -> NdTree p
 insertar p Empty = Node Empty p Empty 0
+insertar p (Node left x right eje) =
+    if comparar eje p x == GT
+    then Node left x (insertar p right) eje
+    else Node (insertar p left) x right eje
+
+-- >>> insertar (P2d (8.0, 9.0)) (Node (Node (Node Empty (P2d (2.0,3.0)) Empty 0) (P2d (5.0,4.0)) (Node Empty (P2d (4.0,7.0)) Empty 0) 1) (P2d (7.0,2.0)) (Node Empty (P2d (8.0,1.0)) (Node Empty (P2d (9.0,6.0)) Empty 0) 1) 0)
+-- Node (Node (Node Empty (P2d (2.0,3.0)) Empty 0) (P2d (5.0,4.0)) (Node Empty (P2d (4.0,7.0)) Empty 0) 1) (P2d (7.0,2.0)) (Node Empty (P2d (8.0,1.0)) (Node (Node Empty (P2d (8.0,9.0)) Empty 0) (P2d (9.0,6.0)) Empty 0) 1) 0
+
+-- Ejercicio 4
+
+buscarReemplazo :: Punto p => NdTree p -> Int -> Bool -> p
+buscarReemplazo (Node Empty p Empty _) eje _ = p
+buscarReemplazo (Node left p right _) eje buscarMenor =
+    if buscarMenor
+    then minimumBy (comparar eje) candidatos
+    else maximumBy (comparar eje) candidatos
+    where candidatos = [buscarReemplazo left eje buscarMenor, p, buscarReemplazo right eje buscarMenor]
+
+reemplazar :: (Eq p, Punto p) => p -> NdTree p -> NdTree p
+reemplazar p (Node Empty x Empty eje) = Empty
+reemplazar p (Node left x Empty eje) = Node (eliminar reemplazo left) reemplazo Empty eje
+    where reemplazo = buscarReemplazo left eje False
+reemplazar p (Node left x right eje) = Node left reemplazo (eliminar reemplazo right) eje
+    where reemplazo = buscarReemplazo right eje True
+
+eliminar :: (Eq p, Punto p) => p -> NdTree p -> NdTree p
+eliminar p Empty = Empty
+eliminar p (Node left x right eje)
+  | p == x = reemplazar p (Node left x right eje)
+  | comparar eje p x == GT = Node left x (eliminar p right) eje
+  | otherwise = Node (eliminar p left) x right eje
+
+-- >>> eliminar (P2d (8.0, 9.0)) (insertar (P2d (8.0, 9.0)) (Node (Node (Node Empty (P2d (2.0,3.0)) Empty 0) (P2d (5.0,4.0)) (Node Empty (P2d (4.0,7.0)) Empty 0) 1) (P2d (7.0,2.0)) (Node Empty (P2d (8.0,1.0)) (Node Empty (P2d (9.0,6.0)) Empty 0) 1) 0))
+-- Node (Node (Node Empty (P2d (2.0,3.0)) Empty 0) (P2d (5.0,4.0)) (Node Empty (P2d (4.0,7.0)) Empty 0) 1) (P2d (7.0,2.0)) (Node Empty (P2d (8.0,1.0)) (Node Empty (P2d (9.0,6.0)) Empty 0) 1) 0
+
+-- Ejercicio 5
+
+type Rect = (Punto2d, Punto2d)
+
+inRegion :: Punto2d -> Rect -> Bool
+inRegion (P2d (px, py)) (P2d (r1x, r1y), P2d (r2x, r2y)) =
+    px >= min r1x r2x
+    && px <= max r1x r2x
+    && py >= min r1y r2y
+    && py <= max r1y r2y
+
+-- >>> inRegion (P2d (100, 99)) (P2d (-99,-99), P2d (99,99))
+-- False
+
+orthogonalSearch :: NdTree Punto2d -> Rect -> [Punto2d]
+orthogonalSearch Empty rect = []
+orthogonalSearch (Node Empty p Empty eje) rect = [p | inRegion p rect]
+orthogonalSearch (Node left p right eje) (r1, r2) =
+    let (min, max) = if comparar eje r1 r2 == GT then (r2, r1) else (r1, r2)
+        resultLeft = if comparar eje min p == GT then [] else orthogonalSearch left (r1, r2)
+        resultRight = if comparar eje max p == GT then orthogonalSearch right (r1, r2) else []
+    in resultLeft ++ [p | inRegion p (r1, r2)] ++ resultRight
+
+-- >>> orthogonalSearch (Node (Node (Node Empty (P2d (2.0,3.0)) Empty 0) (P2d (5.0,4.0)) (Node Empty (P2d (4.0,7.0)) Empty 0) 1) (P2d (7.0,2.0)) (Node Empty (P2d (8.0,1.0)) (Node Empty (P2d (9.0,6.0)) Empty 0) 1) 0) (P2d (3,1),P2d (7,8))
+-- [P2d (5.0,4.0),P2d (4.0,7.0),P2d (7.0,2.0)]
