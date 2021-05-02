a78a2b93d52160b0836c9ea093bc01357183a78d
Lautaro Garavano <lgaravano28@gmail.com>
2021-05-01 22:01:11 -0300

    dolor

--- tp1.hs
+++ tp1.hs
@@ -1,3 +1,8 @@
+import Data.List
+import Data.Maybe
+
+-- Ejercicio 1
+
 data NdTree p = Node (NdTree p) p (NdTree p) Int | Empty
     deriving (Eq, Ord, Show)
 
@@ -5,51 +10,47 @@ class Punto p where
     dimension :: p -> Int
     coord :: Int -> p -> Double
     dist :: p -> p -> Double
-    dist p1 p2 = sum [(coord i p2 - coord i p1)^ 2 | i <- [0..(dimension p1) - 1]] -- robada a castro porque yo me olvide por completo de la existencia de sum
-    -- dist p1 p2 = 
-    --     let f 1 v1 v2 = ((coord 1 v2) - (coord 1 v1))^2
-    --         f n v1 v2 = f (n-1) v1 v2 + ((coord n v2) - (coord n v1))^2
-    --     in
-    --         f m p1 p2
-    --     where
-    --         m = dimension p1 - 1
+    dist p1 p2 = sum [(coord i p2 - coord i p1)^ 2 | i <- [0..dimension p1 - 1]]
 
 newtype Punto2d = P2d (Double, Double) deriving Show
 newtype Punto3d = P3d (Double, Double, Double) deriving Show
 
 instance Punto Punto2d where
     dimension _ = 2
-    coord 0 (P2d (x, y)) = x
-    coord 1 (P2d (x, y)) = y
-    
+    coord 0 (P2d p) = fst p
+    coord 1 (P2d p) = snd p
+
 instance Punto Punto3d where
     dimension _ = 3
-    coord 0 (P3d (x, y, z)) = x
-    coord 1 (P3d (x, y, z)) = y
-    coord 2 (P3d (x, y, z)) = z
+    coord 0 (P3d (x, _, _)) = x
+    coord 1 (P3d (_, y, _)) = y
+    coord 2 (P3d (_, _, z)) = z
 
---2
-{-fromList :: Punto p => [p] -> NdTree p
-fromList [] = Empty
-fromList (p:ps) = -}
+-- EJERCICIO 2
 
-splitPunto :: Punto p => [p] -> ([p], [p])
-splitPunto [] = ([], [])
-splitPunto [x] = ([x], [])
-splitPunto (x:y:zs) = let (xs, ys) = splitPunto zs 
-                 in (x:xs, y:ys)
+ordenar :: Punto p => Int -> [p] -> [p]
+ordenar eje = sortBy (\a b -> compare (coord eje a) (coord eje b))
 
-mergePunto :: Punto p => ([p], [p]) -> Int -> [p]
-mergePunto ([], ys) eje = ys
-mergePunto (xs, []) eje = xs
-mergePunto (x:xs, y:ys) eje = if coord eje x <= coord eje y then x:mergePunto (xs, y:ys) eje else y:mergePunto (x:xs, ys) eje
+mayorMediana :: Punto p => [p] -> Int -> Int
+mayorMediana lista eje = fromMaybe 0 (findIndex (\p -> coord eje p > mediana) lista) - 1
+    where mediana = coord eje (lista!!(length lista `div` 2))
 
-msortPunto :: Punto p => [p] -> Int -> [p]
-msortPunto [] eje = []
-msortPunto [x] eje = [x]
-msortPunto xs eje = let (ls, rs) = splitPunto xs
-               (ls', rs') = (msortPunto ls eje, msortPunto rs eje)
-            in mergePunto (ls', rs') eje
+generarArbol :: Punto p => Int -> [p] -> NdTree p
+generarArbol _ [] = Empty
+generarArbol nivel (x:xs) = Node (generarArbol (nivel + 1) menores) mediana (generarArbol (nivel + 1) mayores) (nivel `mod` dimension x)
+    where
+        lista = ordenar (nivel `mod` dimension x) (x:xs)
+        (menores, mediana:mayores) = splitAt (mayorMediana lista (nivel `mod` dimension x)) lista
 
-{-mediana :: Punto p => [p] -> Int -> p
-mediana xs eje = -}
+fromList :: Punto p => [p] -> NdTree p
+fromList = generarArbol  0
+
+-- >>> fromList [P2d(2,3), P2d(5,4), P2d(9,6), P2d(4,7), P2d(8,1), P2d(7,2)]
+-- >>> fromList [P2d(1,2), P2d(1,3), P2d(2,3), P2d(3,3), P2d(1,4)]
+-- >>> generarArbol 1 [P2d(1,2), P2d(1,3), P2d(2,3), P2d(3,3), P2d(1,4)]
+-- Node (Node (Node Empty (P2d (2.0,3.0)) Empty 0) (P2d (5.0,4.0)) (Node Empty (P2d (4.0,7.0)) Empty 0) 1) (P2d (7.0,2.0)) (Node Empty (P2d (8.0,1.0)) (Node Empty (P2d (9.0,6.0)) Empty 0) 1) 0
+-- Node (Node Empty (P2d (1.0,2.0)) (Node Empty (P2d (1.0,3.0)) Empty 0) 1) (P2d (1.0,4.0)) (Node Empty (P2d (2.0,3.0)) (Node Empty (P2d (3.0,3.0)) Empty 0) 1) 0
+-- Node (Node (Node Empty (P2d (1.0,2.0)) Empty 1) (P2d (1.0,3.0)) (Node Empty (P2d (2.0,3.0)) Empty 1) 0) (P2d (3.0,3.0)) (Node Empty (P2d (1.0,4.0)) Empty 0) 1
+
+insertar :: Punto p => p -> NdTree p -> NdTree p
+insertar p Empty = Node Empty p Empty 0
